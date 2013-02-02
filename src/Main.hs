379f2124ce829f03954f3fbfdf970e462760af15
masaedw <masa.edw@gmail.com>
2013-02-02 17:52:35 +0900

    Add string parser

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,8 @@ module Main (main) where
 
 import Data.Conduit (($=),($$))
 import Data.Conduit as C (runResourceT)
-import Data.Conduit.Binary as CB (sourceHandle, sinkHandle)
+import Data.Conduit.Binary as CB (sourceHandle, sinkHandle, lines)
+import Data.Conduit.List as CL (concatMap)
 import Pudding (conduitPuddingParser, conduitPuddingEvaluator)
 import System.IO
 
--- src/Pudding.hs
+++ src/Pudding.hs
@@ -1,15 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Pudding where
 
-import Control.Applicative ((<|>),(<$>))
+import Control.Applicative ((<|>),(<$>), (*>), (<*))
 import Control.Monad.Trans.Resource (MonadThrow)
-import Data.Attoparsec.ByteString (Parser)
-import Data.Attoparsec.ByteString as A (choice, string, takeWhile)
-import Data.Attoparsec.Char8 as AC (double, isSpace_w8)
-import Data.ByteString.Char8 as BC (ByteString, pack)
+import Data.Attoparsec.ByteString (Parser, try, skip, many')
+import Data.Attoparsec.ByteString as A
+import Data.Attoparsec.Char8 as AC hiding (space)
+import Data.Attoparsec.Combinator (sepBy', manyTill)
+import Data.ByteString.Char8 as BC (ByteString, pack, append)
 import Data.Conduit as C (GLConduit, GLInfConduit, (=$=), mapOutput)
 import Data.Conduit.Attoparsec (conduitParser)
 import Data.Conduit.List as CL (map)
 import Data.Functor ((<$))
+import GHC.Word (Word8)
 
 data PToken = PWord ByteString
             | PNumber Double
@@ -17,15 +20,74 @@ data PToken = PWord ByteString
             | PString ByteString
             deriving (Show, Eq)
 
-parser :: Parser PToken
-parser = PNumber <$> double
+-- | token parser
+--
+-- >>> parseOnly pToken . pack $ "123.5"
+-- Right (PNumber 123.5)
+-- >>> parseOnly pToken . pack $ "\"aabbcc\""
+-- Right (PString "aabbcc")
+-- >>> parseOnly pToken . pack $ "abc"
+-- Right (PWord "abc")
+pToken :: Parser PToken
+pToken = PNumber <$> double
          <|> PBool <$> choice [True <$ string "true"
                               ,False <$ string "false"]
-         <|> PWord <$> (A.takeWhile $ not . isSpace_w8)
+         <|> PString <$> pString
+         <|> PWord <$> (A.takeWhile1 $ not . isSpace_w8)
 
-conduitPuddingParser :: MonadThrow m => GLInfConduit ByteString m PToken
-conduitPuddingParser = mapOutput snd $ conduitParser parser
+-- | string paresr
+--
+-- >>> parseOnly pString . pack $ show ""
+-- Right ""
+-- >>> parseOnly pString . pack $ show "abc"
+-- Right "abc"
+-- >>> parseOnly pString . pack $ show "'"
+-- Right "'"
+-- >>> parseOnly pString . pack $ "\"\\\"\\\\\\0\\a\\b\\f\\n\\r\\t\""
+-- Right "\"\\\NUL\a\b\f\n\r\t"
+pString :: Parser ByteString
+pString = char '"' *> (pack <$> many' (pEscape <|> pChar)) <* char '"'
+
+pChar :: Parser Char
+pChar = AC.satisfy $ AC.notInClass "\"\\"
+
+-- | espace char parser
+--
+-- >>> parseOnly pEscape $ pack "\\\""
+-- Right '"'
+-- >>> parseOnly pEscape $ pack "\\\\"
+-- Right '\\'
+-- >>> parseOnly pEscape $ pack "\\0"
+-- Right '\NUL'
+-- >>> parseOnly pEscape $ pack "\\a"
+-- Right '\a'
+-- >>> parseOnly pEscape $ pack "\\t"
+-- Right '\t'
+pEscape :: Parser Char
+pEscape = char '\\' >> (unEscape <$> (AC.satisfy (`elem` "\"\\0abfnrt")))
+  where
+    unEscape '"' = '"'
+    unEscape '\\' = '\\'
+    unEscape '0' = '\0'
+    unEscape 'a' = '\a'
+    unEscape 'b' = '\b'
+    unEscape 'f' = '\f'
+    unEscape 'n' = '\n'
+    unEscape 'r' = '\r'
+    unEscape 't' = '\t'
+
+space :: Word8 -> Bool
+space w = w == 32 || w == 9
+
+delimiter :: Parser ()
+delimiter = A.skipWhile space
+
+pLine :: Parser [PToken]
+pLine = delimiter *> pToken `sepBy` delimiter <* delimiter <* (endOfLine <|> endOfInput)
+
+conduitPuddingParser :: MonadThrow m => GLInfConduit ByteString m [PToken]
+conduitPuddingParser = mapOutput snd $ conduitParser pLine
 
 -- temporary implementation
-conduitPuddingEvaluator :: Monad m => GLInfConduit PToken m ByteString
-conduitPuddingEvaluator = CL.map $ pack . show
+conduitPuddingEvaluator :: Monad m => GLInfConduit [PToken] m ByteString
+conduitPuddingEvaluator = CL.map $ (`BC.append` "\n") . pack . show
