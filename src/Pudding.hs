c3164161474cefd95d7607dd29337d0c1647e65c
masaedw <masa.edw@gmail.com>
2013-02-09 21:00:40 +0900

    Compile options are set in the cabal file

--- src/Pudding.hs
+++ src/Pudding.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Pudding (
--- src/Pudding/Parse.hs
+++ src/Pudding/Parse.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Pudding.Parse (
   PToken(..),
